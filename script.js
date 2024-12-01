49687b692eb0190c09f95fa0dc062387dc1781b3
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-11-30 21:35:14 -0600

    layout completed

--- script.js
+++ script.js
@@ -33,34 +33,60 @@ document.getElementById('locationSearch').addEventListener('keydown', async (eve
 
         const weatherAPI = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=QVXWFK8AMYT3AR5BL7TCBRRXW`
 
-    // //api url 
-    // const apiURL = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=QVXWFK8AMYT3AR5BL7TCBRRXW`); 
-
         try {
             const apiURL = await fetch (`${weatherAPI}`)
             
             if (!apiURL.ok) {
                 throw new Error(`HTTP error! status: ${apiURL.status}`)}
+                
+            
+            const data = await apiURL.json(); 
+            console.log('Data fetched successfully', data); 
 
-        const data = await apiURL.json(); 
-        console.log('Data fetched successfully', data); 
+            console.log(data.days)
+            
+            const address = document.querySelector('h1'); 
+            address.innerText = data.resolvedAddress; 
+
+            // Converting the date format
+            const rawDate = data.days[0].datetime;
+
+            const [year, month, day] = rawDate.split("-"); // split into components 
+
+            // the new Date() keyword is specifically designed to create Date objects in JS. These objects are part of JS's built-in Date class, which includes specialized methods for working with dates and times. 
+            const formattedDate = new Date(year, month - 1, day).toLocaleDateString("en-US", {
+                 weekday: "long",
+                 month: "long",
+                 day: "numeric",
+                 year: "numeric",
+                });
+
+            const date = document.getElementById('currentDate'); 
+            date.innerText = `${formattedDate}` 
+
+            //temp DOM manipulation 
+
+            const temperature = data.currentConditions.feelslike; 
+
+            const currentTemp = document.getElementById('currentTemp'); 
+
+            currentTemp.innerHTML = `Temp: ${temperature}&deg;F`
+
+            //conditions DOM manipulation 
+            const condition = data.currentConditions.conditions; 
+
+            const currentCondition = document.getElementById('currentCondition'); 
+
+            currentCondition.innerHTML = `Condition: ${condition}`
+
+
         console.log('Address:', data.resolvedAddress); 
         console.log('Conditions:', data.currentConditions.conditions); 
         console.log('Feels like:', `${data.currentConditions.feelslike} degrees`); 
-        // Converting the date format
-        const rawDate = data.days[0].datetime;
-        // the new Date() keyword is specifically designed to create Date objects in JS. These ojbects are part of JS's built-in Date class, which includes specialized methods for working with dates and times. 
-        const formattedDate = new Date(rawDate).toLocaleDateString("en-US", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-        });
-        console.log('Date:', formattedDate);
+    
     } catch (error) {
         console.error('Error fetching data:', error.message)
     }
-
 }
 }); 
 
